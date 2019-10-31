11e9cdbe9d2499504b8c4367a6fb32816ed89b83
Ricardo Fragoso <ricardfragoso@gmail.com>
2019-10-31 18:08:22 -0300

    pandas

--- teste1.py
+++ teste1.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+
+
+# Hello world of dash with plotly
+# Basics to plot a dashboard
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -28,14 +33,18 @@ app.layout = html.Div(children=[
              ),
 
     dcc.Graph(
-        id='example-graph',
+        id='example-graph-2',
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
             }
         }
     )
